391b07496ea992cb3b0e8885d360297669e5fee1
James Graham <J.Graham@software.ac.uk>
2020-04-08 09:46:52 +0100

    Move controller into entry script

--- patientdb.py
+++ patientdb.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
-"""Software for managing patient data in our imaginary hospital.
-
-This module is the entry-point to the software, it simply collects
-arguments from the command line and passes them to the controller.
-"""
+"""Software for managing patient data in our imaginary hospital."""
 
 import argparse
 
-from inflammation import controller
+from inflammation import patient, plotting
+
+
+def main(args):
+    """The MVC Controller of the patient data system.
+
+    The Controller is responsible for:
+    - selecting the necessary models and views for the current task
+    - passing data between models and views
+    """
+    infiles = args.infiles
+    if not isinstance(infiles, list):
+        infiles = [args.infiles]
+
+    for filename in infiles:
+        inflammation_data = patient.load_csv(filename)
+        plotting.visualize(inflammation_data)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -20,4 +33,4 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    controller.controller(args)
+    main(args)
