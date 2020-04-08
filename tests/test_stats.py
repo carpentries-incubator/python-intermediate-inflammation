368c372859833e9ff710cf8cf350b42427b67845
James Graham <J.Graham@software.ac.uk>
2020-04-08 13:55:32 +0100

    Add tests for stats code

---
+++ tests/test_stats.py
@@ -0,0 +1,33 @@
+"""Tests for statistics functions within the Model layer."""
+
+import numpy as np
+import numpy.testing as npt
+
+
+def test_daily_mean_zeros():
+    """Test that mean function works for an array of zeros."""
+    from inflammation.models import daily_mean
+
+    # NB: the comment 'yapf: disable' disables automatic formatting using
+    # a tool called 'yapf' which we have used when creating this project
+    test_array = np.array([[0, 0],
+                           [0, 0],
+                           [0, 0]])  # yapf: disable
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(np.array([0, 0]), daily_mean(test_array))
+
+
+def test_daily_mean_integers():
+    """Test that mean function works for an array of positive integers."""
+    from inflammation.models import daily_mean
+
+    test_array = np.array([[1, 2],
+                           [3, 4],
+                           [5, 6]])  # yapf: disable
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(np.array([3, 4]), daily_mean(test_array))
+
+
+# TODO(lesson-robust) Implement tests for the other statistical functions
