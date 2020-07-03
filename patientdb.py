c4a381be34c2bfc5f0aa0321b1b96b3668fa9821
Aleksandra Nenadic <a.nenadic@manchester.ac.uk>
2020-07-03 09:49:04 +0100

    Intentionally introduced style mistakes.

--- patientdb.py
+++ patientdb.py
@@ -17,17 +17,13 @@ def main(args):
     if not isinstance(infiles, list):
         infiles = [args.infiles]
 
+
     for filename in infiles:
         inflammation_data = models.load_csv(filename)
 
-        view_data = {
-            'average': models.daily_mean(inflammation_data),
-            'max': models.daily_max(inflammation_data),
-            'min': models.daily_min(inflammation_data),
-        }
+        view_data = {'average': models.daily_mean(inflammation_data), 'max': models.daily_max(inflammation_data), 'min': models.daily_min(inflammation_data)}
 
         views.visualize(view_data)
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
