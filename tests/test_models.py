3d6cf334933079554f908151f6cc7e9d8ec235b7
Steve Crouch <s.crouch@software.ac.uk>
2021-05-07 10:59:46 +0100

    Make test arrays names consistent

--- tests/test_models.py
+++ tests/test_models.py
@@ -10,24 +10,26 @@ def test_daily_mean_zeros():
 
     # NB: the comment 'yapf: disable' disables automatic formatting using
     # a tool called 'yapf' which we have used when creating this project
-    test_array = np.array([[0, 0],
+    test_input = np.array([[0, 0],
                            [0, 0],
                            [0, 0]])  # yapf: disable
+    test_result = np.array([0, 0])
 
     # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(np.array([0, 0]), daily_mean(test_array))
+    npt.assert_array_equal(test_result, daily_mean(test_input))
 
 
 def test_daily_mean_integers():
     """Test that mean function works for an array of positive integers."""
     from inflammation.models import daily_mean
 
-    test_array = np.array([[1, 2],
+    test_input = np.array([[1, 2],
                            [3, 4],
                            [5, 6]])  # yapf: disable
+    test_result = np.array([3, 4])
 
     # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(np.array([3, 4]), daily_mean(test_array))
+    npt.assert_array_equal(test_result, daily_mean(test_input))
 
 
 # TODO(lesson-robust) Implement tests for the other statistical functions
