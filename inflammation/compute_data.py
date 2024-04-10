ca0e5b8ba9932c40533e67f4a3dbbc29ba93b286
Aleksandra Nenadic <a.nenadic@manchester.ac.uk>
2024-04-10 10:37:54 +0100

    Added full analysis code

---
+++ inflammation/compute_data.py
@@ -0,0 +1,31 @@
+"""Module containing mechanism for calculating standard deviation between datasets.
+"""
+
+import glob
+import os
+import numpy as np
+
+from inflammation import models, views
+
+
+def analyse_data(data_dir):
+    """Calculate the standard deviation by day between datasets
+
+    Gets all the inflammation csvs within a directory, works out the mean
+    inflammation value for each day across all datasets, then graphs the
+    standard deviation of these means."""
+    data_file_paths = glob.glob(os.path.join(data_dir, 'inflammation*.csv'))
+    if len(data_file_paths) == 0:
+        raise ValueError(f"No inflammation csv's found in path {data_dir}")
+    data = map(models.load_csv, data_file_paths)
+
+
+    means_by_day = map(models.daily_mean, data)
+    means_by_day_matrix = np.stack(list(means_by_day))
+
+    daily_standard_deviation = np.std(means_by_day_matrix, axis=0)
+
+    graph_data = {
+        'standard deviation by day': daily_standard_deviation,
+    }
+    views.visualize(graph_data)
