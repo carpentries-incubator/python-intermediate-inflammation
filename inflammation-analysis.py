37cdcc9cd45d3b6f739e197e0b92955e5e84b7a5
jakobchristiaanse <jakob.christiaanse@live.de>
2024-04-15 14:30:11 +0200

    Code style fixes.

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -2,35 +2,35 @@
 """Software for managing and analysing patients' inflammation data in our imaginary hospital."""
 
 import argparse
-
 from inflammation import models, views
 
 
-def main(args):
+def main(arguments):
     """The MVC Controller of the patient inflammation data system.
 
     The Controller is responsible for:
     - selecting the necessary models and views for the current task
     - passing data between models and views
     """
-    InFiles = args.infiles
-    if not isinstance(InFiles, list):
-        InFiles = [args.infiles]
+    in_files = arguments.in_files
+    if not isinstance(in_files, list):
+        in_files = [arguments.in_files]
 
-
-    for filename in InFiles:
+    for filename in in_files:
         inflammation_data = models.load_csv(filename)
-
-        view_data = {'average': models.daily_mean(inflammation_data), 'max': models.daily_max(inflammation_data), 'min': models.daily_min(inflammation_data)}
-
+        view_data = {'average': models.daily_mean(inflammation_data),
+                     'max': models.daily_max(inflammation_data),
+                     'min': models.daily_min(inflammation_data)
+                     }
         views.visualize(view_data)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='A basic patient inflammation data management system')
 
     parser.add_argument(
-        'infiles',
+        'in_files',
         nargs='+',
         help='Input CSV(s) containing inflammation series for each patient')
 
