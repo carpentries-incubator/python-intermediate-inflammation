5e7793ac03caae18b7549cb58a10b59866c0e760
kirsten-bulsink <kirstenmariekebulsink@gmail.com>
2024-04-15 15:29:45 +0200

    docstring extensions

--- inflammation/models.py
+++ inflammation/models.py
@@ -19,16 +19,28 @@ def load_csv(filename):
 
 
 def daily_mean(data):
-    """Calculate the daily mean of a 2D inflammation data array."""
+    """Calculate the daily mean of a 2D inflammation data array.
+
+    :param data: 2D inflammation data array.
+    :returns: An array of mean values of measurements for each day.
+    """
     return np.mean(data, axis=0)
 
 
 def daily_max(data):
-    """Calculate the daily max of a 2D inflammation data array."""
+    """Calculate the daily max of a 2D inflammation data array.
+    
+    :param data: 2D inflammation data array.
+    :returns: An array of mean values of measurements for each day.
+    """
     return np.max(data, axis=0)
 
 
 def daily_min(data):
-    """Calculate the daily min of a 2D inflammation data array."""
+    """Calculate the daily min of a 2D inflammation data array.
+    
+    :param data: 2D inflammation data array.
+    :returns: An array of mean values of measurements for each day.
+    """
     return np.min(data, axis=0)
 
