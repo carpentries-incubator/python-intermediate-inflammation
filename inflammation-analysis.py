ef9e24ce0c8a6fa8de9d7b976feb2faf9dba4d1f
Job Rosier <job.rosier@vu.nl>
2024-04-16 14:13:24 +0200

    added std to view_data in inflammation-analysis.py

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -22,7 +22,8 @@ def main(args):
 
         view_data = {'average': models.daily_mean(inflammation_data),
                      'max': models.daily_max(inflammation_data),
-                     'min': models.daily_min(inflammation_data)
+                     'min': models.daily_min(inflammation_data),
+                     'std': models.daily_std(inflammation_data)
                      }
 
         views.visualize(view_data)
