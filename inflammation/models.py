91ddc1d179c8316b133f4a2c77531a348b1cbc66
kirsten-bulsink <kirstenmariekebulsink@gmail.com>
2024-04-16 14:30:25 +0200

    added normalized fun and test

--- inflammation/models.py
+++ inflammation/models.py
@@ -2,20 +2,20 @@
 
 The Model layer is responsible for the 'business logic' part of the software.
 
-Patients' data is held in an inflammation table (2D array) where each row contains 
-inflammation data for a single patient taken over a number of days 
+Patients' data is held in an inflammation table (2D array) where each row contains
+inflammation data for a single patient taken over a number of days
 and each column represents a single day across all patients.
 """
 
 import numpy as np
 
 
-def load_csv(filename):  
+def load_csv(filename):
     """Load a Numpy array from a CSV
 
     :param filename: Filename of CSV to load
     """
-    return np.loadtxt(fname=filename, delimiter=',')
+    return np.loadtxt(fname=filename, delimiter=",")
 
 
 def daily_mean(data):
@@ -29,7 +29,7 @@ def daily_mean(data):
 
 def daily_max(data):
     """Calculate the daily max of a 2D inflammation data array.
-    
+
     :param data: 2D inflammation data array.
     :returns: An array of mean values of measurements for each day.
     """
@@ -38,9 +38,18 @@ def daily_max(data):
 
 def daily_min(data):
     """Calculate the daily min of a 2D inflammation data array.
-    
+
     :param data: 2D inflammation data array.
     :returns: An array of mean values of measurements for each day.
     """
     return np.min(data, axis=0)
 
+
+def patient_normalise(data):
+    """Normalise patient data from a 2D inflammation data array.
+
+    :param data: 2D inflammation data array.
+    :returns: An array of normalized values of measurements for each day.
+    """
+    max = np.max(data, axis=1)
+    return data / max[:, np.newaxis]
--- tests/test_models.py
+++ tests/test_models.py
@@ -51,3 +51,15 @@ def test_daily_min_string():
 
     with pytest.raises(TypeError):
         error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
+
+
+@pytest.mark.parametrize(
+    "test_norm, expected_norm",
+    [
+        ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]])
+    ])
+def test_patient_normalise(test_norm, expected_norm):
+    """Test normalisation works for arrays of one and positive integers.
+       Assumption that test accuracy of two decimal places is sufficient."""
+    from inflammation.models import patient_normalise
+    npt.assert_almost_equal(patient_normalise(np.array(test_norm)), np.array(expected_norm), decimal=2)
