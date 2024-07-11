4f42ba30ef024aeb7244d6c5add1f268f928d15a
Claudia C <claudia.chien@charite.de>
2024-07-11 09:38:44 +0200

    style changes

--- inflammation/models.py
+++ inflammation/models.py
@@ -10,56 +10,25 @@ and each column represents a single day across all patients.
 import numpy as np
 
 
-def load_csv(filename):
+def load_csv(filename):  
     """Load a Numpy array from a CSV
 
     :param filename: Filename of CSV to load
     """
-    return np.loadtxt(fname=filename, delimiter=",")
+    return np.loadtxt(fname=filename, delimiter=',')
 
 
 def daily_mean(data):
-    """Calculate the daily mean of a 2D inflammation data array.
-
-    :param data: a 2D data array
-    :returns: the mean"""
+    """Calculate the daily mean of a 2d inflammation data array."""
     return np.mean(data, axis=0)
 
 
 def daily_max(data):
-    """Calculate the daily max of a 2D inflammation data array.
-
-    :param data: a 2D data array
-    :returns: the max"""
+    """Calculate the daily max of a 2d inflammation data array."""
     return np.max(data, axis=0)
 
 
 def daily_min(data):
-    """Calculate the daily min of a 2D inflammation data array.
-
-    :param data: a 2D data array
-    :returns: the min"""
+    """Calculate the daily min of a 2d inflammation data array."""
     return np.min(data, axis=0)
 
-
-def patient_normalise(data):
-    """
-    Normalise patient data from a 2D inflammation data array.
-
-    NaN values are ignored, and normalised to 0.
-
-    Negative values are rounded to 0.
-    """
-    if not isinstance(data, np.ndarray):
-        raise TypeError('data input should be ndarray')
-    if len(data.shape) != 2:
-        raise ValueError('inflammation array should be 2-dimensional')
-    if np.any(data < 0):
-        raise ValueError('Inflammation values should not be negative')
-
-    datmx = np.max(data, axis=1)
-    with np.errstate(invalid='ignore', divide='ignore'):
-        normalised = data / datmx[:, np.newaxis]
-    normalised[np.isnan(normalised)] = 0
-    normalised[normalised < 0] = 0
-    return normalised
