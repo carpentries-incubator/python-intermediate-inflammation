a1c160c409faf471d6d24065baa6d80dc6b40ad7
Clayton Roberts <claytonr@mac-232.local>
2024-09-24 12:28:38 +0200

    Add docstrings to functions in models module.

--- inflammation/models.py
+++ inflammation/models.py
@@ -19,16 +19,28 @@ def load_csv(filename):
 
 
 def daily_mean(data):
-    """Calculate the daily mean of a 2D inflammation data array."""
+    """Calculate the daily mean of a 2D inflammation data array.
+
+    :param data: 2D table of patient data.
+    :returns: Mean value of data across all rows, i.e., mean for each patient.
+    """
     return np.mean(data, axis=0)
 
 
 def daily_max(data):
-    """Calculate the daily max of a 2D inflammation data array."""
+    """Calculate the daily max of a 2D inflammation data array.
+
+    :param data: 2D table of patient data.
+    :returns: Max value of data across all rows, i.e., max for each patient.
+    """
     return np.max(data, axis=0)
 
 
 def daily_min(data):
-    """Calculate the daily min of a 2D inflammation data array."""
+    """Calculate the daily min of a 2D inflammation data array.
+
+    :param data: 2D table of patient data.
+    :returns: Min value of data across all rows, i.e., min for each patient.
+    """
     return np.min(data, axis=0)
 
