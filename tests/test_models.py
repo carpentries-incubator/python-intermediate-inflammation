142507b679ce66ca647612d2dd3ead98c3d6008f
Clayton Roberts <claytonr@mac-232.local>
2024-10-01 12:13:07 +0200

    Add error catching in python testing.

--- tests/test_models.py
+++ tests/test_models.py
@@ -94,3 +94,28 @@ def test_daily_min_string():
 
     with pytest.raises(TypeError):
         daily_min([['Hello', 'there'], ['General', 'Kenobi']])
+
+
+@pytest.mark.parametrize(
+    "test, expected, expect_raises",
+    [
+        ([[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]], None),
+        ([[1, 1, 1], [1, 1, 1], [1, 1, 1]], [[1, 1, 1], [1, 1, 1], [1, 1, 1]], None),
+        ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]], None),
+        ([[np.nan, 2, 3], [0, 3, 1], [4, -2, 1]], [[0, 0.66, 1], [0, 1, 0.33], [1, 0, 0.25]], ValueError),
+        ([[-1, 2, 3], [4, 5, 6], [7, 8, 9]],
+        [[0, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+        ValueError),
+    ])
+def test_patient_normalise(test, expected, expect_raises):
+    """Test normalisation works for arrays of one and positive integers.
+       Test with a relative and absolute tolerance of 0.01."""
+    from inflammation.models import patient_normalise
+
+    if expect_raises is not None:
+        with pytest.raises(expect_raises):
+            result = patient_normalise(np.array(test))
+            npt.assert_allclose(result, np.array(expected), rtol=1e-2, atol=1e-2)
+    else:
+        result = patient_normalise(np.array(test))
+        npt.assert_allclose(result, np.array(expected), rtol=1e-2, atol=1e-2)
