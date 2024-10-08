c997b3a66c6883b52fdae77e008ceb8948941096
langtonhugh <s.h.langton@amsterdamumc.nl>
2024-10-08 09:59:11 +0200

    python3 to python in main.yml

--- inflammation/models.py
+++ inflammation/models.py
@@ -44,9 +44,9 @@ def patient_normalise(data):
     if np.any(data < 0):
         raise ValueError('Inflammation values should not be negative')
 
-    max = np.nanmax(data, axis=1)
+    max_data = np.nanmax(data, axis=1)
     with np.errstate(invalid='ignore', divide='ignore'):
-        normalised = data / max[:, np.newaxis]
+        normalised = data / max_data[:, np.newaxis]
     normalised[np.isnan(normalised)] = 0
     normalised[normalised < 0] = 0
     return normalised
