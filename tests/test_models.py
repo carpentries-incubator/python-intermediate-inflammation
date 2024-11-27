1a3a5ce721d4218cfb7f205ce8869cda3ee3cfe2
RGB43 <60193418+RGB43@users.noreply.github.com>
2024-11-27 11:29:33 +0000

    add github actions

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,10 +2,22 @@
 
 import numpy as np
 import numpy.testing as npt
+import pytest
 
-from inflammation.models import daily_mean
+from inflammation.models import daily_mean, daily_max, daily_min
 
-def test_daily_mean_zeros():
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[0,0],[0,0],[0,0]],[0,0]),
+        ([[0, 0],[0, 0],[0, 0]], [0, 0]),
+        ([[-1, -1],[-1, -1],[-1, -1]],[-1, -1]),
+        ([[1, 2],[3, 4],[5, 6]],[3, 4]),
+        ([[1, 2],[3, 4],[5, 6]], [6, 7])
+    ]
+)
+#not sure why pytest seems to be required here. error says it should be test but doesn't seem to work
+def pytest_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
 
     test_input = np.array([[0, 0],
@@ -46,3 +58,34 @@ def test_daily_mean():
     test_result = np.array([3, 4])
 
     npt.assert_array_equal(daily_mean(test_input), test_result)
+"""
+def test_daily_mean_string():
+    test_input = np.array([["this is something incorrect", 2],
+                           [3, 4],
+                           [5, 6]])
+    test_result = np.array([3, 4])
+
+    npt.assert_array_equal(daily_mean(test_input), test_result)
+"""
+
+
+"""
+def test_daily_min_string():
+    #test for typeerror when passing strings
+
+    #with pytest.raises(TypeError):
+     #   error_expected = daily_min([["one", "two"],["3","4"]])
+"""
+
+
+"""Tests for the Patient model - class/file in this folder
+
+from inflammation.models import Patient
+
+def test_create_patient():
+
+    name = 'Alice'
+    p = Patient(name=name)
+
+    assert p.name == name
+"""
