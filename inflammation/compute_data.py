43be41941f23d72b6a018b78cd5ed55c6d4ce13c
Steven C Chan <80276802+SCChan21@users.noreply.github.com>
2025-01-17 10:58:29 +0000

    Abstracted compute_data csv loading

--- inflammation/compute_data.py
+++ inflammation/compute_data.py
@@ -28,9 +28,15 @@ def compute_daily_sdev(data):
     return compute_sdev(means_by_day_matrix)
 
 
-class CSVStuff:
-    def __init__(self, csv_file_list):
+class CSVDataSrc:
+    def __init__(self, data_dir):
+        csv_file_list = glob.glob(os.path.join(data_dir, 'inflammation*.csv'))
+        csv_file_list.sort()
+        self.data_dir = data_dir
+        if len(csv_file_list) == 0:
+            raise ValueError(f"No inflammation data CSV files found in path {data_dir}")
         self.csv_filenames = csv_file_list
+        print(self.csv_filenames[0], self.csv_filenames[-1])
 
     def load_data(self):
         return map(models.load_csv, self.csv_filenames)
@@ -43,11 +49,7 @@ def analyse_data(data_dir):
     works out the mean inflammation value for each day across all datasets,
     then plots the graphs of standard deviation of these means."""
 
-    data_file_paths = glob.glob(os.path.join(data_dir, 'inflammation*.csv'))
-    if len(data_file_paths) == 0:
-        raise ValueError(f"No inflammation data CSV files found in path {data_dir}")
-
-    data = CSVStuff(data_file_paths).load_data()
+    data = CSVDataSrc(data_dir).load_data()
     daily_standard_deviation = compute_daily_sdev(data)
 
     graph_data = {
