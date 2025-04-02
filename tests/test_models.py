f2eef213f4a11cd80373bb4a4f9cd8f1b4be359a
= <=>
2025-04-02 13:53:02 +0100

    patient nomralise

--- tests/test_models.py
+++ tests/test_models.py
@@ -5,7 +5,7 @@ import numpy.testing as npt
 import pytest
 
 
-from inflammation.models import daily_mean, daily_max, daily_min
+from inflammation.models import daily_mean, daily_max, daily_min, patient_normalise
 
 @pytest.mark.parametrize(
     "test, expected",
@@ -46,23 +46,14 @@ def test_daily_min_string():
     with pytest.raises(TypeError):
         error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
 
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]])
+    ])
+def test_patient_normalise(test, expected):
+    """Test normalisation works for arrays of one and positive integers.
+       Test with a relative and absolute tolerance of 0.01."""
 
-# def test_daily_max():
-#     test_input = np.array([[1, 10],
-#                            [3, 4],
-#                            [5, 6]])
-#     test_result = [5,10]
-#     npt.assert_array_equal(daily_max(test_input), test_result)
-
-
-# def test_daily_min():
-#     test_input = np.array([[1, 10],
-#                            [3, 4],
-#                            [5, 6]])
-#     test_result = [1,4]
-#     npt.assert_array_equal(daily_min(test_input), test_result)
-
-
-
-
-
+    result = patient_normalise(np.array(test))
+    npt.assert_allclose(result, np.array(expected), rtol=1e-2, atol=1e-2)
