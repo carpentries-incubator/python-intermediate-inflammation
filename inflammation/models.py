668457e6f375d5ad5340d3c4911850e6df8ae138
Aleksandra Nenadic <a.nenadic@manchester.ac.uk>
2025-07-14 15:50:24 +0100

    Add more tests and Pylint run to build

--- inflammation/models.py
+++ inflammation/models.py
@@ -46,3 +46,21 @@ def daily_mean(data):
     :returns: An array of mean values of measurements for each day.
     """
     return np.mean(data, axis=0)
+
+
+def patient_normalise(data):
+    """
+    Normalise patient data from a 2D inflammation data array.
+
+    NaN values are ignored, and normalised to 0.
+
+    Negative values are rounded to 0.
+    """
+    if np.any(data < 0):
+        raise ValueError('Inflammation values should not be negative')
+    max = np.nanmax(data, axis=1)
+    with np.errstate(invalid='ignore', divide='ignore'):
+        normalised = data / max[:, np.newaxis]
+    normalised[np.isnan(normalised)] = 0
+    normalised[normalised < 0] = 0
+    return normalised
--- tests/test_models.py
+++ tests/test_models.py
@@ -3,7 +3,7 @@
 import numpy as np
 import numpy.testing as npt
 import pytest
-from inflammation.models import daily_max, daily_mean, daily_min
+from inflammation.models import daily_max, daily_mean, daily_min, patient_normalise
 
 
 from inflammation.models import daily_mean
@@ -41,3 +41,28 @@ def test_daily_max(test, expected):
 def test_daily_min(test, expected):
     """Test min function works for zeroes, positive integers, mix of positive/negative integers."""
     npt.assert_array_equal(daily_min(np.array(test)), np.array(expected))
+
+
+@pytest.mark.parametrize(
+    "test, expected, expect_raises",
+    [
+        (
+            [[-1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            [[0, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+            ValueError,
+        ),
+        (
+            [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+            None,
+        )
+    ])
+def test_patient_normalise(test, expected, expect_raises):
+    """Test normalisation works for arrays of one and positive integers."""
+        
+    if expect_raises is not None:
+        with pytest.raises(expect_raises):
+            patient_normalise(np.array(test))
+    else:
+        result = patient_normalise(np.array(test))
+        npt.assert_allclose(result, np.array(expected), rtol=1e-2, atol=1e-2)
