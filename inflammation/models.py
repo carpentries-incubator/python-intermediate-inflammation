7572b86b7d86f8c94b24dd00bda8beab61560c26
Aleksandra Nenadic <a.nenadic@manchester.ac.uk>
2025-07-14 14:18:42 +0100

    Add pylint and improved code style.

--- inflammation/models.py
+++ inflammation/models.py
@@ -10,7 +10,7 @@ and each column represents a single day across all patients.
 import numpy as np
 
 
-def load_csv(filename):  
+def load_csv(filename):
     """Load a Numpy array from a CSV
 
     :param filename: Filename of CSV to load
@@ -19,27 +19,30 @@ def load_csv(filename):
 
 
 def daily_min(data):
-   """Calculate the daily minimum of a 2D inflammation data array for each day.
-
-   :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
-   :returns: An array of minimum values of measurements for each day.
-   """
-   return np.min(data, axis=0)
+    """Calculate the daily minimum of a 2D inflammation data array for each day.
+    
+    :param data: A 2D data array with inflammation data (each row contains 
+    measurements for a single patient across all days).
+    :returns: An array of minimum values of measurements for each day.
+    """
+    return np.min(data, axis=0)
 
 
 def daily_max(data):
-   """Calculate the daily maximum of a 2D inflammation data array for each day.
+    """Calculate the daily maximum of a 2D inflammation data array for each day.
 
-   :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
-   :returns: An array of max values of measurements for each day.
-   """
-   return np.max(data, axis=0)
+    :param data: A 2D data array with inflammation data (each row contains 
+    measurements for a single patient across all days).
+    :returns: An array of max values of measurements for each day.
+    """
+    return np.max(data, axis=0)
 
 
 def daily_mean(data):
-   """Calculate the daily mean of a 2D inflammation data array for each day.
+    """Calculate the daily mean of a 2D inflammation data array for each day.
 
-   :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
-   :returns: An array of mean values of measurements for each day.
-   """
-   return np.mean(data, axis=0)
+    :param data: A 2D data array with inflammation data (each row contains 
+    measurements for a single patient across all days).
+    :returns: An array of mean values of measurements for each day.
+    """
+    return np.mean(data, axis=0)
